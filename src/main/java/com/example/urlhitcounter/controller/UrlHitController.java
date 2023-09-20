7cefa02ba49a11dc528bb4d5221cc22f2f3cf4e4
Bhag chandra <bkrajpoot08021997@gmail.com>
2023-09-20 19:59:19 +0530

    complete url

--- src/main/java/com/example/urlhitcounter/controller/UrlHitController.java
+++ src/main/java/com/example/urlhitcounter/controller/UrlHitController.java
@@ -1,25 +1,37 @@
 package com.example.urlhitcounter.controller;
 
-
 import com.example.urlhitcounter.service.UrlHitService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+
 @RestController
 @RequestMapping("api/v1/visitor-count-app")
-
 public class UrlHitController {
-
     @Autowired
+    private UrlHitService urlHitService;
 
-    public UrlHitService urlHitService;
-
-    @GetMapping("/count")
-    public ResponseEntity<Integer> getHitCount(){
-        int count = urlHitService.getHitCount();
-        return ResponseEntity.ok(count);
+    @GetMapping("count")
+    public ResponseEntity<Map<String, Integer>> getHitCount() {
+        int count = urlHitService.incrementHitCount();
+        Map<String, Integer> response = new HashMap<>();
+        response.put("visitors", count);
+        return ResponseEntity.ok(response);
+    }
+    @GetMapping("username/{username}/count")
+    public ResponseEntity<Map<String, Object>> getHitCountByUsername(@PathVariable String username) {
+        int count = urlHitService.incrementHitCountByUsername(username);
+        Map<String, Object> response = new HashMap<>();
+        response.put("username", username);
+        response.put("hitcount", count);
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/example/urlhitcounter/service/UrlHitService.java
+++ src/main/java/com/example/urlhitcounter/service/UrlHitService.java
@@ -2,17 +2,22 @@ package com.example.urlhitcounter.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class UrlHitService {
-
     private int hitCount = 0;
-
-    public int getHitCount() {
-        return hitCount;
+    private Map<String, Integer> hitCounts = new HashMap<>();
+    public int incrementHitCount() {
+        return ++hitCount;
     }
 
-    public void incrementHitCount() {
-        hitCount++;
+    public int incrementHitCountByUsername(String username) {
+        hitCounts.putIfAbsent(username, 0);
+        int count = hitCounts.get(username);
+        count++;
+        hitCounts.put(username, count);
+        return count;
     }
-
 }
